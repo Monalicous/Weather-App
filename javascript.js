b3c6e29a41cce0d254638a213ad55bb2b28d34bd
Monalicous <samukelisiwentusi@outlook.com>
2024-11-18 21:43:00 +0400

    Done the search functionality using API

--- javascript.js
+++ javascript.js
@@ -1,21 +1,27 @@
-function refreshWeather(response) {
-    console.log(response.data.current.temperature);
+function updateWeatherData(response) {
+    let weatherTemp = document.querySelector("#weather-temp");
+    let temperature = response.data.temperature.current
+    let cityElement = document.querySelector("#thecity");
+
+    cityElement.innerHTML = response.data.city;
+    weatherTemp.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
     let apiKey = "9a9b6ao3534d1b2afb7b49dddt30a6e7";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
-    axios.get(apiUrl).then(refreshWeather);
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(updateWeatherData);
 }
 
 function formSubmit(event) {
     event.preventDefault();
     let city = document.querySelector("#input");
-    let cityElement = document.querySelector("#thecity");
-    cityElement.innerHTML = city.value;
     searchCity(city.value);
 }
+//When user search for a city
 
 let searchFormElement = document.querySelector("#search_city");
 searchFormElement.addEventListener("submit", formSubmit);
+
+searchCity("Midrand");
 
