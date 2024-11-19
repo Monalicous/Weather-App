807afa974f9df9e1e0fe478a3899e35c52e34a54
Monalicous <samukelisiwentusi@outlook.com>
2024-11-19 08:40:40 +0400

    injected more weather data

--- javascript.js
+++ javascript.js
@@ -2,9 +2,32 @@ function updateWeatherData(response) {
     let weatherTemp = document.querySelector("#weather-temp");
     let temperature = response.data.temperature.current
     let cityElement = document.querySelector("#thecity");
+    let descriptionElement = document.querySelector("#condition");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`
     weatherTemp.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satureday"];
+
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`
 }
 
 function searchCity(city) {
